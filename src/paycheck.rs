3da7c67e473fd00cd1eb0a2e448c6245faa18de8
Khushboo <khushboobhattu@gmail.com>
2021-05-05 19:00:56 -0500

    paycheck checked

--- src/paycheck.rs
+++ src/paycheck.rs
@@ -22,8 +22,11 @@ pub struct PaycheckState {
     state_address: String,
     medicare: i32,
     state_tax: i32,
+    social_sec: i32,
+    bracket_tax: i32,
     four_one_k: i32,
     insurance_premium: i32,
+    report: i32,
     report_year: i32,
     logout_button: button::State
 }
@@ -37,19 +40,27 @@ impl PaycheckState {
             PaycheckMessage::paycheck_report(e_id, report_year) => {
                 let employee =client.query_one("SELECT * FROM employee WHERE E_ID = $1", &[&e_id])
                     .expect("Can't find employee!");
-                let statetax = client.query("SELECT stateTax($1, $2)", &[&e_id, &report_year]);
-                let brackettax = client.query("SELECT bracket($1, $2)", &[&e_id, &report_year]);
-                let four_one_k = client.query("SELECT Val401k($1)", &[&e_id]);
-                let social_sec = client.query("SELECT socialSec($1, $2)", &[&e_id, &report_year]);
-                let insurance = client.query("SELECT insurance_premium($1, $2)", &[&e_id, &report_year]);
-                let medicare = client.query("SELECT medicare($1, $2)", &[&e_id, &report_year]);
-                let paycheck = client.query("SELECT paycheck($1, $2)", &[&e_id, &report_year]);
+                let statetax = client.query_one("SELECT stateTax($1, $2)", &[&e_id, &report_year]);
+                let brackettax = client.query_one("SELECT bracket($1, $2)", &[&e_id, &report_year]);
+                let four_one_k = client.query_one("SELECT Val401k($1)", &[&e_id]);
+                let social_sec = client.query_one("SELECT socialSec($1, $2)", &[&e_id, &report_year]);
+                let insurance = client.query_one("SELECT insurance_premium($1, $2)", &[&e_id, &report_year]);
+                let medicare = client.query_one("SELECT medicare($1, $2)", &[&e_id, &report_year]);
+                let paycheck = client.query_one("SELECT paycheck($1, $2)", &[&e_id, &report_year]);
                 self.e_id = employee.get("E_ID");
                 self.ssn = employee.get("SSN");
                 self.first_name = employee.get("firstName");
                 self.last_name = employee.get("lastName");
                 self.state_address = employee.get( "stateAddress");
                 self.report_year = report_year;
+                self.state_tax = statetax.unwrap().get("tax_val");
+                self.four_one_k = four_one_k.unwrap().get("contribution");
+                self.bracket_tax = brackettax.unwrap().get("bracket_val");
+                self.social_sec = social_sec.unwrap().get("ssn_val");
+                self.insurance_premium = insurance.unwrap().get("premiumcost");
+                self.medicare = medicare.unwrap().get("contribution");
+                self.report = paycheck.unwrap().get("paycheck_amount");
+
                 /*println!("Employee Name: {} {}", self.first_name, self.last_name);
                 println!("ssn: {} ", self.ssn);
                 println!("Tax Deductions: {:?} [state tax], {:?} [federal tax], {:?} [social security] {:?} [medicare]", statetax, brackettax, social_sec, medicare);
@@ -65,23 +76,26 @@ impl PaycheckState {
         Column::new()
             .push(Row::new()
                 .push(Text::new("Employee Name:"))
-                .push(Text::new(&*self.first_name)))
+                .push(Text::new(&self.first_name)))
             .push(Row::new()
                 .push(Text::new("SSN: "))
-                .push(Text::new(&*self.ssn)))
-            // .push(Row::new()
-            //     .push(Text::new("Tax Deductions: "))
-            //     .push(Text::new(&self.statetax))
-            //     .push(Text::new(&self.brackettax)))
-            // .push(Row::new()
-            //     .push(Text::new("401k contribution: "))
-            //     .push(Text::new(&self.fouronek)))
-            // .push(Row::new()
-            //     .push(Text::new("Insurance Premium contribution: "))
-            //     .push(Text::new(&self.insurance)))
-            // .push(Row::new()
-            //     .push(Text::new("EMPLOYEE PAYCHECK: "))
-            //     .push(Text::new(&self.paycheck)))
+                .push(Text::new(&self.ssn)))
+            .push(Row::new()
+                .push(Text::new("Tax Deductions: "))
+                .push(Text::new(format!("{:?}", self.state_tax)))
+                .push(Text::new(format!("{:?}", self.bracket_tax))))
+            .push(Row::new()
+                .push(Text::new("401k contribution: "))
+                .push(Text::new(format!("{:?}", self.four_one_k))))
+            .push(Row::new()
+                .push(Text::new("Social Security contribution: "))
+                .push(Text::new(format!("{:?}", self.social_sec))))
+            .push(Row::new()
+                .push(Text::new("Insurance Premium contribution: "))
+                .push(Text::new(format!("{:?}", self.insurance_premium))))
+            .push(Row::new()
+                .push(Text::new("EMPLOYEE PAYCHECK: "))
+                .push(Text::new(format!("{:?}", self.report))))
             .push(match user.usertype {
                 UserType::Manager => {
                     Button::new(&mut self.logout_button, Text::new("Log Out"))
